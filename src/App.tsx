4acb2ddeaf2c6e66a6643e89671da275bd261ca8
Brett Bilyk <brettbilyk@gmail.com>
2023-11-15 14:19:28 -0700

    Move chakra implementation from App,tsx to main.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,16 @@
 // import { useState } from 'react'
 import './App.css'
-import { ChakraBaseProvider, extendBaseTheme } from '@chakra-ui/react'
-import { theme as chakraTheme } from "@chakra-ui/theme"
-import Details from './Details'
+import Details from './components/Details'
+import Title from './components/Title'
 
-const {
-  Card,
-  Heading,
-  Button,
-} = chakraTheme.components
-
-const theme = extendBaseTheme({
-  components: {
-    Button,
-    Card,
-    Heading,
-  },
-})
 
 function App() {
 
   return (
-    <ChakraBaseProvider theme={theme}>
+    <>
+      <Title />
       <Details />
-    </ChakraBaseProvider>
+    </>
   )
 }
 
--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,27 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
+import { theme as chakraTheme } from "@chakra-ui/theme"
+import { ChakraBaseProvider, extendBaseTheme } from '@chakra-ui/react'
+
+const {
+  Card,
+  Heading,
+  Button,
+} = chakraTheme.components
+
+const theme = extendBaseTheme({
+  components: {
+    Button,
+    Card,
+    Heading,
+  },
+})
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <ChakraBaseProvider theme={theme}>
+      <App />
+    </ChakraBaseProvider>
   </React.StrictMode>,
 )
