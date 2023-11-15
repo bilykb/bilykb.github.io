4d7ecb37c9c5412d6a78185bd9fba142ae94146a
Brett Bilyk <brettbilyk@gmail.com>
2023-11-15 15:16:05 -0700

    Update title component

---
+++ src/components/Title.tsx
@@ -0,0 +1,20 @@
+// import { useState } from 'react'
+import {
+  Box,
+  Heading,
+  Text,
+} from '@chakra-ui/react'
+
+function Title() {
+
+  return (
+    <Box maxW='32rem'>
+      <Heading mb={4}>Brett Bilyk</Heading>
+      <Text fontSize='xl'>
+        Passionate and versatile Full Stack Developer, with a strong drive for continuous learning. Adept at problem-solving, taking initiative, and collaborating within a team environment. Equipped with a diverse skill set and the ability to adapt to any software stack. Experienced working in a full stack (frontend and backend) and software development cycle in the Agile framework. Committed to being an effective Developer who is engaging, resourceful, and not afraid to ask questions.
+      </Text>
+    </Box>
+  )
+}
+
+export default Title
