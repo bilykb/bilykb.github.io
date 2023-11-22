d6d32d536ec4db0af00de5d05833618a89ad7387
Brett Bilyk <brettbilyk@gmail.com>
2023-11-22 15:52:26 -0700

    Remove center component

--- src/App.tsx
+++ src/App.tsx
@@ -2,16 +2,14 @@
 import './App.css'
 import Details from './components/Details'
 import Title from './components/Title'
-import { Flex, Center } from '@chakra-ui/react'
+import { Flex } from '@chakra-ui/react'
 
 function App() {
 
   return (
     <Flex align={'center'} direction={'column'}>
-      {/* <Center w={'100rem'} h={'100rem'}> */}
-        <Title />
-        <Details />
-      {/* </Center> */}
+      <Title />
+      <Details />
     </Flex>
   )
 }
