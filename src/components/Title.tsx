c6a9387b3a1f95703761e6ef84516514f5dff182
Brett Bilyk <brettbilyk@gmail.com>
2023-11-23 12:50:17 -0700

    Adjust title Margins

--- src/components/Title.tsx
+++ src/components/Title.tsx
@@ -10,7 +10,7 @@ import Portrait from './Portrait'
 function Title() {
 
   return (
-    <Flex maxW={'75%'} m={'10%'} justify={'center'}>
+    <Flex maxW={'75%'} mt={'10%'} mb={'4%'} justify={'center'}>
       <Portrait />
       <Box maxW={'25%'}>
         <Heading mb={4}>Brett Bilyk</Heading>
@@ -18,7 +18,6 @@ function Title() {
           Passionate and versatile Full Stack Developer, with a strong drive for continuous learning. Adept at problem-solving, taking initiative, and collaborating within a team environment. Equipped with a diverse skill set and the ability to adapt to any software stack. Experienced working in a full stack (frontend and backend) and software development cycle in the Agile framework. Committed to being an effective Developer who is engaging, resourceful, and not afraid to ask questions.
         </Text>
       </Box>
-
     </Flex>
   )
 }
